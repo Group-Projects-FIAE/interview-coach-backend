bcfe4b511ae3701f2c177167fc4d9a68c86b2ca0
Tim Gollub <tg.timgollub@gmail.com>
2025-02-19 12:06:12 +0100

    Added basic prompting functionality

--- .idea/src/main.py
+++ .idea/src/main.py
@@ -1,12 +1,20 @@
 from fastapi import FastAPI
+import setup_llama
 
 app = FastAPI()
 
 file = open('system_prompt.txt', 'r')
-SYSTEM_PROMPT = file.read()
-print(SYSTEM_PROMPT)
+SYSTEM_PROMPT = f"<<SYS>>\n{file.read()}\n<</SYS>>"
+model = setup_llama.setup_model(8, 512, 2048)
+
+def chat_with_llama(prompt):
+    final_prompt = f"[INST] {SYSTEM_PROMPT}\n\n{prompt} [/INST]"
+    response = model(final_prompt, max_tokens=200)
+    return {"response": response["choices"][0]["text"]}
+
+print(chat_with_llama("Hello!"))
+
 
 @app.get("/")
 def read_root():
     return {"message": "Hello, FastAPI!"}
-
--- .idea/src/setup_llama.py
+++ .idea/src/setup_llama.py
@@ -2,10 +2,9 @@ from llama_cpp import Llama
 
 def setup_model(core_count, batch_size, context_size):
     llm = Llama(
-        model_path=".idea\src\Meta-Llama-3.1-8B-Instruct-Q4_K_M.gguf",
+        model_path="Meta-Llama-3.1-8B-Instruct-Q4_K_M.gguf",
         n_threads=core_count,  # CPU Cores used
         n_batch=batch_size,  # Batch size, should be a square of 2
         n_ctx=context_size    # Context size, should be a square of 2
     )
     return llm
-
