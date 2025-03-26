5bdcec9302a0c34ac884c2fc161c7c152bc34d33
Tim Gollub <tg.timgollub@gmail.com>
2025-03-26 10:55:32 +0100

    Added scripts to create and retrieve extracted notes and user preferences.

--- src/database/extracted_notes.py
+++ src/database/extracted_notes.py
@@ -1,44 +1,40 @@
 import mariadb
-import time
 from database import setup_maria_db
+import time
 
-def create_session(session_id: str, user_id: str):
+
+def create_extracted_note(session_id: int, note_text: str):
     try:
         connection = setup_maria_db.get_db_connection(setup_maria_db.DB_NAME)
         cursor = connection.cursor()
 
-        query = """INSERT INTO Sessions (session_id, user_id, start_time)
-        VALUES (%s, %s, FROM_UNIXTIME(%s))"""
-        values = (session_id, user_id, time.time())
-
+        query = """INSERT INTO ExtractedNotes (session_id, note_text, created_at) VALUES (%s, %s, %s, FROM_UNIXTIME(%s))"""
+        values = (session_id, note_text, time.time())
         cursor.execute(query, values)
 
         connection.commit()
 
     except mariadb.Error as e:
-        print("Error while trying to insert new session:", e)
+        print("Error while trying to insert new extracted note:", e)
     finally:
         if connection:
             cursor.close()
             connection.close()
 
-def get_job_description(session_id: str):
+def get_extracted_note(session_id: str):
     try:
         connection = setup_maria_db.get_db_connection(setup_maria_db.DB_NAME)
         cursor = connection.cursor()
 
-        query = "SELECT * FROM Sessions WHERE session_id = %s"
+        query = "SELECT * FROM ExtractedNotes WHERE session_id = %s"
         cursor.execute(query, (session_id,))
 
         connection.commit()
         return cursor.fetchall()
     
     except mariadb.Error as e:
-        print("Error while trying to retrieve session:", e)
+        print("Error while trying to retrieve extracted notes:", e)
     finally:
         if connection:
             cursor.close()
             connection.close()
-
-#create_session("0", "cbe0c97f-4552-4ef4-8b25-358060737016")
-#setup_maria_db.print_table("Sessions")
--- src/database/user_preferences.py
+++ src/database/user_preferences.py
@@ -1,44 +1,39 @@
 import mariadb
-import time
 from database import setup_maria_db
 
-def create_session(session_id: str, user_id: str):
+def create_user_preference(user_id: int, darkmode: str, language: str):
     try:
         connection = setup_maria_db.get_db_connection(setup_maria_db.DB_NAME)
         cursor = connection.cursor()
 
-        query = """INSERT INTO Sessions (session_id, user_id, start_time)
-        VALUES (%s, %s, FROM_UNIXTIME(%s))"""
-        values = (session_id, user_id, time.time())
-
+        query = """INSERT INTO UserPreferences (user_id, darkmode, language) VALUES (%s, %s, %s)"""
+        values = (user_id, darkmode, language)
         cursor.execute(query, values)
 
         connection.commit()
 
     except mariadb.Error as e:
-        print("Error while trying to insert new session:", e)
+        print("Error while trying to insert new user preference:", e)
     finally:
         if connection:
             cursor.close()
             connection.close()
 
-def get_job_description(session_id: str):
+def get_user_preference(user_id: str):
     try:
         connection = setup_maria_db.get_db_connection(setup_maria_db.DB_NAME)
         cursor = connection.cursor()
 
-        query = "SELECT * FROM Sessions WHERE session_id = %s"
-        cursor.execute(query, (session_id,))
+        query = "SELECT * FROM UserPreference WHERE user_id = %s"
+        cursor.execute(query, (user_id,))
 
         connection.commit()
         return cursor.fetchall()
     
     except mariadb.Error as e:
-        print("Error while trying to retrieve session:", e)
+        print("Error while trying to retrieve user preference:", e)
     finally:
         if connection:
             cursor.close()
             connection.close()
 
-#create_session("0", "cbe0c97f-4552-4ef4-8b25-358060737016")
-#setup_maria_db.print_table("Sessions")
