f7f76b5f5408cac05599320de40e7ee4ef25e7da
Tim Gollub <tg.timgollub@gmail.com>
2025-04-03 09:46:52 +0200

    Changed Mode selection behaviour

--- src/prompt/prompt.py
+++ src/prompt/prompt.py
@@ -2,19 +2,24 @@ file = open('system_prompt.txt', 'r')
 SYSTEM_PROMPT = file.read()
 file.close()
 
+CURRENT_MODE = None
+
 def getSystemPrompt(user_input: str):
     system_prompt: str = SYSTEM_PROMPT
     formated_input = user_input.lower()
 
     if("/interview" in formated_input):
-        file = open('interview_prompt.txt', 'r')
-        interview_prompt = file.read()
-        file.close()
-        system_prompt += interview_prompt
-        return system_prompt
+        CURRENT_MODE = "interview"
     elif("/quiz" in formated_input):
-        return system_prompt
+        CURRENT_MODE = "quiz"
     elif("/training" in formated_input):
-        return system_prompt
-    else:
-        return system_prompt
+        CURRENT_MODE = "training"
+    
+    file = open(CURRENT_MODE + '_prompt.txt', 'r')
+    mode_prompt = file.read()
+    file.close()
+
+    if(CURRENT_MODE != None):
+        system_prompt += mode_prompt
+    
+    return system_prompt
